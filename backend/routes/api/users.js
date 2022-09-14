8b564cceb3075fa5da531622f23a58877c2870a9
Derek Emsbach <demsbach92@gmail.com>
2022-09-13 22:20:57 -0600

    added first and lasy name to User route

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -8,6 +8,12 @@ const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
 const validateSignup = [
+  check('firstName')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a first name.'),
+  check('lastName')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a last name.'),
   check('email')
     .exists({ checkFalsy: true })
     .isEmail()
