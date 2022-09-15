8562d5ee8ce154b12cf52df15a5b558c67c06419
Derek Emsbach <demsbach92@gmail.com>
2022-09-14 22:31:16 -0600

    Database models and Migrations added

---
+++ backend/db/migrations/20220915031024-create-image.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Images', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      url: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      reviewImagesId: {
+        type: Sequelize.INTEGER
+      },
+      spotImagesId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Images');
+  }
+};
