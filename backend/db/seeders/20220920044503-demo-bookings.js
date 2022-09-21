6da93e47d1c1ab2fb5702759efbdf49f4ffd7d5d
Derek Emsbach <demsbach92@gmail.com>
2022-09-20 18:43:52 -0600

    added remaining seeders

--- backend/db/seeders/20220920044503-demo-bookings.js
+++ backend/db/seeders/20220920044503-demo-bookings.js
@@ -2,23 +2,29 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    return queryInterface.bulkInsert('Bookings', [
+      {
+        spotId: 1,
+        userId: 2,
+        startDate:"2022-04-20",
+        endDate:"2022-04-21"
+      },
+      {
+        spotId: 2,
+        userId: 3,
+        startDate:"2022-07-22",
+        endDate:"2022-07-25"
+      },
+      {
+        spotId: 3,
+        userId: 1,
+        startDate:"2022-08-04",
+        endDate:"2022-08-06"
+      },
+    ]);
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete('Bookings', null, {});
   }
 };
--- backend/db/seeders/20220920044517-demo-reviews.js
+++ backend/db/seeders/20220920044517-demo-reviews.js
@@ -2,23 +2,24 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('Reviews',[
+      {
+        userId: 1,
+        spotId: 1,
+        review: "meh, it was ok..",
+        stars: 3,
+      },
+      {
+        userId: 2,
+        spotId: 2,
+        review: "Beter than I could have imagined",
+        stars: 5,
+      },
+
+  ])
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Reviews',null,{})
   }
 };
